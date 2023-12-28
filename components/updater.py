bdb92029e881dc44b741603bf491864c220e75f6
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2023-12-29 00:37:24 +0100

    Working version of updater

--- components/updater.py
+++ components/updater.py
@@ -3,20 +3,23 @@ import tkinter as tk
 from tkinter import ttk
 import webbrowser
 
-
 # ANNOTATION:
 # Value "name" in all tkinter functions
 # navigate to the name of master window
 
+running_version = "genesis-1.0.0"
+
+
 def update_link():
     webbrowser.open("https://github.com/ziomciopoziomcio/TextStreamManager/releases")
+
 
 # Function to download name of latest version
 def version_download():
     try:
         response = requests.get(
             "https://raw.githubusercontent.com/ziomciopoziomcio/TextStreamManager/main/assets/version.txt")
-        if response == 200:
+        if response.status_code == 200:
             return response.text
         else:
             return -1
@@ -52,11 +55,21 @@ def error(name):
     label = tk.Label(updater, text="We cannot download the latest version.\nCheck your internet connection")
     label.pack(pady=10)
     updater.mainloop()
+
+
 # # Brain of update system
-# def update_controller():
+def update_controller(name):
+    downloaded_version = version_download()
+    if downloaded_version == -1:
+        error(name)
+    elif downloaded_version == running_version:
+        uptodate(name, running_version)
+    else:
+        new_version(name, running_version, downloaded_version)
+
 
 # Test function
-if __name__ == '__main__':
-    main = tk.Tk()
-    new_version(main, "1.0.0", "1.0.1")
-    main.mainloop()
+# if __name__ == '__main__':
+#     main = tk.Tk()
+#     update_controller(main)
+#     main.mainloop()
