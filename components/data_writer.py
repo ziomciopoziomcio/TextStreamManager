145e094f833e28a1eb1e6c86512022e64ee1e69f
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2023-12-30 18:34:08 +0100

    name changes

--- components/data_writer.py
+++ components/data_writer.py
@@ -1,19 +1,4 @@
 import json
-import os
-
-# Function to trace file with variables
-def variable_path():
-    try:
-        current_directory = os.getcwd()
-        assets_directory = os.path.join(current_directory, '../assets')
-        filename_in_assets = 'variables.json'
-        variable_location = os.path.join(assets_directory, filename_in_assets)
-        if not os.path.exists(variable_location):
-            raise FileNotFoundError(1)
-        return variable_location
-    except Exception:
-        return 1
-
 
 # Function to write version from data file
 def version_writer(path, to_save):
